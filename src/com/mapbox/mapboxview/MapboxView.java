d1bf1cab96b024f7893de895633be08564bbdf54
Nolan <nolan.m.smith@vanderbilt.edu>
2014-04-09 19:04:02 -0600

    Mapbox working and showing

--- src/com/mapbox/mapboxview/MapboxView.java
+++ src/com/mapbox/mapboxview/MapboxView.java
@@ -1,20 +1,48 @@
 package com.mapbox.mapboxview;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Random;
 
-import android.content.Context;
-import android.util.AttributeSet;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.GoogleMap.OnMapLoadedCallback;
 import com.google.android.gms.maps.model.*;
 
-public class MapboxView extends MapView {
+public class MapboxView extends MapView implements OnMapLoadedCallback {
+	
+	private final String TAG = this.getClass().getSimpleName();
+	
 	private String mapID;
+	private GoogleMap gmap;
+	private LatLngBounds bounds;
+	private String url = "";
 	
 	public MapboxView(Context context) {
 		super(context);
@@ -35,30 +63,13 @@ public class MapboxView extends MapView {
 	public MapboxView(Context context, String mapID) {
 		this(context);
 		
-		this.mapID = mapID;
+		mapID = "musiccitycenter.vqloko6r";
 		
+		url = String.format(Locale.ENGLISH, "https://a.tiles.mapbox.com/v3/%s.json", mapID);
 		// TODO - Get JSON tile map from format(Locale.ENGLISH, "https://a.tiles.mapbox.com/v3/%s.json", this.mapID)
-		HashMap<String, Object> tileMap = null;
 		
-		ArrayList<Number> centerArrayList = (ArrayList<Number>) tileMap.get("center");
-		LatLng centerCoordinate = new LatLng(centerArrayList.get(1).doubleValue(), centerArrayList.get(0).doubleValue());
-		
-		ArrayList<Number> boundsArrayList = (ArrayList<Number>) tileMap.get("bounds");
-		LatLng northeastCoordinate = new LatLng(((Number) boundsArrayList.get(3)).doubleValue(), ((Number) boundsArrayList.get(2)).doubleValue());
-		LatLng southwestCoordinate = new LatLng(((Number) boundsArrayList.get(1)).doubleValue(), ((Number) boundsArrayList.get(0)).doubleValue());
-		LatLngBounds bounds = new LatLngBounds(southwestCoordinate, northeastCoordinate);
-		
-		int minZoom = ((Number) tileMap.get("minzoom")).intValue();
-		int maxZoom = ((Number) tileMap.get("maxzoom")).intValue();
-		
-		GoogleMap map = getMap();
-		// TODO - No idea if 5 is a good padding number - I just chose one
-		map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 5));
-		
-		// TODO - Set the correct width, height
-		MapboxTileProvider tileProvider = new MapboxTileProvider(200, 200, this.mapID, minZoom, maxZoom);
-		
-		map.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
+		gmap = getMap();
+		new TileLoader().execute(new WeakReference<MapboxView>(this));
 	}
 	
 	String getMapID() {
@@ -83,7 +94,9 @@ public class MapboxView extends MapView {
 			String[] letters = { "a", "b", "c", "d" };
 			int randomIndex = new Random().nextInt(4);
 			
-			String URLString = String.format(Locale.ENGLISH, "https://%s.tiles.mapbox.com/v3/%s/%d/%d/%d%s.%s", letters[randomIndex], this.mapID, zoom, x, y, "2x", "png");
+			String URLString = String.format(Locale.ENGLISH, 
+					"https://%s.tiles.mapbox.com/v3/%s/%d/%d/%d%s.%s", 
+					letters[randomIndex], this.mapID, zoom, x, y, "2x", "png");
 			URL url = null;
 			
 			if (checkTileExists(x, y, zoom)) {
@@ -109,5 +122,149 @@ public class MapboxView extends MapView {
 
 			return tileExists;
 		}
+	}
+	
+	private class TileLoader extends 
+		AsyncTask<WeakReference<MapboxView>, Void, HashMap<String, Object> > {
+		
+		@Override
+		protected void onPreExecute() {
+			//setProgressVisibility(true, "Retrieving Tiles", "Shouldn't be long now!");
+			
+		}
+		
+
+		@Override
+		protected HashMap<String, Object> doInBackground(WeakReference<MapboxView>... handlers) {
+			
+			HashMap<String, Object> tileMap = new HashMap<String, Object>();
+			try {
+				
+				Log.d(TAG, "URL: " + url);
+				String tileContent = getContent(url);
+				JSONObject tileJSON = new JSONObject(tileContent);
+				
+				Log.d(TAG, "JSON retreived");
+				
+				JSONArray centerJSON = tileJSON.getJSONArray("center");
+				Log.d(TAG, "center: " + centerJSON.toString());
+				ArrayList<Number> centerArr = new ArrayList<Number>();
+				for(int i = 0; i < centerJSON.length(); ++i) {
+					centerArr.add(centerJSON.optDouble(i));
+				}
+				
+				ArrayList<Number> boundsArr = new ArrayList<Number>();
+				JSONArray boundsJSON = tileJSON.getJSONArray("bounds");
+				Log.d(TAG, "bounds: " + boundsJSON.toString());
+				for(int i = 0; i < boundsJSON.length(); ++i) {
+					boundsArr.add(boundsJSON.optDouble(i));
+				}
+				
+				Integer minZoom = tileJSON.getInt("minzoom");
+				Integer maxZoom = tileJSON.getInt("maxzoom");
+				
+				Log.d(TAG, "Center: " + centerArr.toString());
+				Log.d(TAG, "Bounds: " + boundsArr.toString());
+				Log.d(TAG, "minZoom: " + minZoom + " maxZoom: " + maxZoom);
+				
+				tileMap.put("center", centerArr);
+				tileMap.put("bounds", boundsArr);
+				tileMap.put("minzoom", minZoom);
+				tileMap.put("maxzoom", maxZoom);
+				
+				ArrayList<Number> centerArrayList = (ArrayList<Number>) tileMap.get("center");
+				LatLng centerCoordinate = new LatLng(centerArrayList.get(1).doubleValue(), centerArrayList.get(0).doubleValue());
+				
+				ArrayList<Number> boundsArrayList = (ArrayList<Number>) tileMap.get("bounds");
+				LatLng northeastCoordinate = new LatLng(((Number) boundsArrayList.get(3)).doubleValue(), ((Number) boundsArrayList.get(2)).doubleValue());
+				LatLng southwestCoordinate = new LatLng(((Number) boundsArrayList.get(1)).doubleValue(), ((Number) boundsArrayList.get(0)).doubleValue());
+				bounds = new LatLngBounds(southwestCoordinate, northeastCoordinate);
+				
+				//int minZoom = ((Number) tileMap.get("minzoom")).intValue();
+				//int maxZoom = ((Number) tileMap.get("maxzoom")).intValue();
+				
+				// TODO - Set the correct width, height
+				MapboxTileProvider tileProvider = new MapboxTileProvider(200, 200, mapID, minZoom, maxZoom);
+				
+				gmap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
+				
+				Log.d(TAG, "spinning");
+				while(gmap == null) {};
+				handlers[0].get().setGMapCallback();
+				Log.d(TAG, "done spinning");
+				
+				return tileMap;
+				
+			} catch (ClientProtocolException e) {
+				Log.d(TAG, "ClientProtocol Exception thrown");
+				/** handle specific exceptions */
+			} catch (IOException e) {
+				Log.d(TAG, "IO Exception thrown");
+				/** handle specific exceptions */
+			} catch (JSONException e) {
+				Log.d(TAG, "JSON Exception thrown");
+				/** handle specific exceptions */
+			}
+			
+			return null;
+		}	
+		
+		
+		@Override
+		protected void onPostExecute(HashMap<String, Object> tileMap) {			
+			//setProgressVisibility(false, null, null);
+			/*
+			ArrayList<Number> centerArrayList = (ArrayList<Number>) tileMap.get("center");
+			LatLng centerCoordinate = new LatLng(centerArrayList.get(1).doubleValue(), centerArrayList.get(0).doubleValue());
+			
+			ArrayList<Number> boundsArrayList = (ArrayList<Number>) tileMap.get("bounds");
+			LatLng northeastCoordinate = new LatLng(((Number) boundsArrayList.get(3)).doubleValue(), ((Number) boundsArrayList.get(2)).doubleValue());
+			LatLng southwestCoordinate = new LatLng(((Number) boundsArrayList.get(1)).doubleValue(), ((Number) boundsArrayList.get(0)).doubleValue());
+			bounds = new LatLngBounds(southwestCoordinate, northeastCoordinate);
+			
+			int minZoom = ((Number) tileMap.get("minzoom")).intValue();
+			int maxZoom = ((Number) tileMap.get("maxzoom")).intValue();
+			
+			// TODO - Set the correct width, height
+			MapboxTileProvider tileProvider = new MapboxTileProvider(200, 200, mapID, minZoom, maxZoom);
+			
+			gmap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));*/
+		}	
+		
+	}
+	
+	private String getContent(String url) throws ClientProtocolException, IOException {
+		StringBuilder sb = new StringBuilder();
+
+		HttpClient client = new DefaultHttpClient();
+		HttpParams httpParams = client.getParams();
+		HttpConnectionParams.setConnectionTimeout(httpParams, 3000);
+		HttpConnectionParams.setSoTimeout(httpParams, 5000);
+		HttpResponse response = client.execute(new HttpGet(url));
+		HttpEntity entity = response.getEntity();
+		
+		if (entity != null) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					entity.getContent(), "UTF-8"), 8192);
+
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+			reader.close();
+		}
+		
+		return sb.toString();
+	}
+	
+	private void setGMapCallback() {
+		gmap.setOnMapLoadedCallback(this);
+	}
+	
+	@Override
+	public void onMapLoaded() {
+		
+		// TODO - No idea if 5 is a good padding number - I just chose one
+		gmap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 5));
 	}
 }
